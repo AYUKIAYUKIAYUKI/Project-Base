962a4aa5ed6d35e8d6cc419a06cf554d4f71b61b
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2024-06-17 16:48:01 +0900

    ゲームモードに応じ画面遷移が行えるようになりました。(仮に、タイトル→インゲーム→リザルトの流れでループ可能です)

--- ugoku/Resource/manager.h
+++ ugoku/Resource/manager.h
@@ -16,7 +16,6 @@
 #include "light.h"
 #include "input.h"
 #include "sound.h"
-#include "scene.h"
 
 //****************************************************
 // マネージャークラス
@@ -33,14 +32,12 @@ public:
 	void Update();	// 更新処理
 	void Draw();	// 描画処理
 
-	static CRenderer* GetRenderer();			// レンダラー取得
-	static CCamera* GetCamera();				// カメラ取得
-	static CLight* GetLight();					// ライト取得
-	static CInputKeyboard* GetKeyboard();		// キーボード取得
-	static CInputPad* GetPad();					// パッド取得
-	static CSound* GetSound();					// サウンド取得
-	static CScene* GetScene();					// シーン取得
-	static void SetScene(CScene::MODE mode);	// シーン設定
+	static CRenderer* GetRenderer();		// レンダラー取得
+	static CCamera* GetCamera();			// カメラ取得
+	static CLight* GetLight();				// ライト取得
+	static CInputKeyboard* GetKeyboard();	// キーボード取得
+	static CInputPad* GetPad();				// パッド取得
+	static CSound* GetSound();				// サウンド取得
 
 private:
 
@@ -50,7 +47,6 @@ private:
 	static CInputKeyboard* m_pKeyboard;	// キーボード管理
 	static CInputPad* m_pPad;			// パッド管理
 	static CSound* m_pSound;			// サウンド管理
-	static CScene* m_pScene;			// シーン管理
 };
 
 #endif // _MANAGER_H_
--- ugoku/Resource/renderer.cpp
+++ ugoku/Resource/renderer.cpp
@@ -12,6 +12,17 @@
 #include "manager.h"
 #include "object.h"
 
+// 仮
+#include "bg.h"
+#include "block.h"
+#include "block3D.h"
+#include "enemy.h"
+#include "field.h"
+#include "item.h"
+#include "player.h"
+#include "player3D.h"
+#include "score.h"
+
 //============================================================================
 // コンストラクタ
 //============================================================================
@@ -121,6 +132,58 @@ HRESULT CRenderer::Init(HWND hWnd, BOOL bWindiw)
 	// テクスチャ読み込み
 	m_pTexture->Load();
 
+#if 0
+
+	// 背景の生成 (仮)
+	CBg::Create(
+		{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f },	// 中心位置
+		{ SCREEN_HEIGHT * 0.5f,  SCREEN_HEIGHT * 0.5f, 0.0f });	// サイズ
+
+	// ブロックの生成 (仮)
+	CBlock::Create(
+		{ SCREEN_WIDTH * 0.25f, SCREEN_HEIGHT * 0.7f, 0.0f },	// 中心位置
+		{ 50.0f, 100.0f, 0.0f });								// サイズ
+
+	// ブロックの生成 (仮)
+	CBlock::Create(
+		{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f },	// 中心位置
+		{ 75.0f, 75.0f, 0.0f });								// サイズ
+
+	// ブロックの生成 (仮)
+	CBlock::Create(
+		{ SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.3f, 0.0f },	// 中心位置
+		{ 100.0f, 50.0f, 0.0f });								// サイズ
+
+	for (int i = 0; i < 5; i++)
+	{
+		// ブロックの生成 (仮)
+		CBlock::Create(
+			{ SCREEN_WIDTH * 0.95f, SCREEN_HEIGHT * 0.9f + (-70.0f * i), 0.0f },	// 中心位置
+			{ 30.0f, 30.0f, 0.0f });												// サイズ
+
+		// ブロックの生成 (仮)
+		CBlock::Create(
+			{ SCREEN_WIDTH * 0.95f + (-70.0f * i), SCREEN_HEIGHT * 0.9f, 0.0f },	// 中心位置
+			{ 30.0f, 30.0f, 0.0f });												// サイズ
+	}
+
+	// エネミーの生成 (仮)
+	CEnemy::Create(
+		{ SCREEN_WIDTH * 0.0f, SCREEN_HEIGHT * 1.0f, 0.0f },	// 中心位置
+		{ 50.0f, 50.0f, 0.0f });								// サイズ
+
+	// プレイヤーの生成 (仮)
+	CPlayer::Create(
+		{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT, 0.0f },	// 中心位置
+		{ 40.0f, 50.0f, 0.0f });						// サイズ
+
+	// アイテムの生成 (仮)
+	CItem::Create(
+		{ SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.25f, 0.0f },	// 中心位置
+		{ 80.0f, 80.0f, 0.0f });								// サイズ
+
+#else
+
 	// モデルインスタンスを生成
 	m_pModel = DBG_NEW CModel;
 
@@ -131,6 +194,65 @@ HRESULT CRenderer::Init(HWND hWnd, BOOL bWindiw)
 
 	// モデル読み込み
 	m_pModel->Load();
+
+	// 3Dプレイヤーの生成 (仮)
+	CPlayer3D::Create(
+		{ 0.0f, 00.0f, 0.0f });	// 中心位置
+
+	// 地面の生成 (仮)
+	CField::Create(
+		{ 0.0f, 0.0f, 0.0f },		// 中心位置
+		{ 100.0f, 0.0f, 100.0f });	// サイズ
+
+	for (int i = 0; i < 5; i++)
+	{
+		// 3Dブロックの生成 (仮)
+		CBlock3D::Create(
+			{ 100.0f, 0.0f, 100.0f + (-25.0f * i) });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+		CBlock3D::Create(
+			{ 100.0f + (-25.0f * i), 0.0f, 100.0f });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+		CBlock3D::Create(
+			{ -100.0f, 0.0f, -100.0f + (25.0f * i) });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+		CBlock3D::Create(
+			{ -100.0f + (25.0f * i), 0.0f, -100.0f });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+		CBlock3D::Create(
+			{ 0.0f , 0.0f + (25.0f * i), -100.0f });	// 中心位置
+	}
+
+	// 3Dブロックの生成 (仮)
+	CBlock3D::Create(
+		{ -40.0f, 0.0f, 150.0f });	// 中心位置
+
+	// 3Dブロックの生成 (仮)
+	CBlock3D::Create(
+		{ -20.0f, 20.0f, 150.0f });	// 中心位置
+
+	// 3Dブロックの生成 (仮)
+	CBlock3D::Create(
+		{ 0.0f, 40.0f, 150.0f });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+	CBlock3D::Create(
+		{ 20.0f, 60.0f, 150.0f });	// 中心位置
+
+		// 3Dブロックの生成 (仮)
+	CBlock3D::Create(
+		{ 40.0f, 80.0f, 150.0f });	// 中心位置
+
+#endif
+
+	// スコアの生成 (仮)
+	CScore::Create(
+		{ 25.0f, 30.0f, 0.0f },	// 中心位置
+		25.0f);					// 数列の配置間隔
 
 	return S_OK;
 }
@@ -198,9 +320,6 @@ void CRenderer::Draw()
 	{
 		// カメラをセット
 		CManager::GetCamera()->SetCamera();
-
-		// シーンの専用描画
-		CManager::GetScene()->Draw();
 
 		// 全オブジェクト描画処理
 		CObject::DrawAll();
