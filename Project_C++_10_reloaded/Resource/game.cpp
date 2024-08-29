ca36c8494a260d273f8d5cc7a027a6adfd99b05d
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2024-08-29 17:34:36 +0900

    リリース環境での動作を確認しました。

--- Project_C++_10_reloaded/Resource/game.cpp
+++ Project_C++_10_reloaded/Resource/game.cpp
@@ -78,7 +78,7 @@ void CGame::Update()
 		// ステージデバッグ画面へ
 		CFakeScreen::GetInstance()->SetFade(MODE::STAGE);
 	}
-	else if (CManager::GetKeyboard()->GetTrigger(DIK_SPACE))
+	else if (CManager::GetKeyboard()->GetTrigger(DIK_BACK))
 	{
 		// リザルト画面へ
 		CFakeScreen::GetInstance()->SetFade(MODE::RESULT);
