b1b41c60bd7d9b0f10b1d90699fa250d088d0f47
AYUKIAYUKIAYUKI <231018@st.yoshida-g.ac.jp>
2024-09-02 10:27:11 +0900

    2Dオブジェクトに色の情報を追加し、UI挙動用のクラスを追加しました。

--- Project_C++_10_reloaded/Resource/object_2D.h
+++ Project_C++_10_reloaded/Resource/object_2D.h
@@ -39,6 +39,12 @@ public:
 	D3DXVECTOR3 GetSize();			// サイズ取得
 	void SetSize(D3DXVECTOR3 size);	// サイズ設定
 
+	D3DXCOLOR GetCol();			// 色取得
+	void SetCol(D3DXCOLOR col);	// 色設定
+
+	float& GetAlpha();				// アルファ値取得
+	void SetAlpha(float fAlpha);	// アルファ値設定
+
 	float GetLength();	// 展開用対角線取得
 
 	void SetTexWidth(float fWidth);		// 横テクスチャ分割幅設定
@@ -50,9 +56,6 @@ public:
 	int GetNowPatternV();					// 現在のテクスチャ縦分割幅取得
 	void SetNowPatternV(int nNowPatternV);	// 現在のテクスチャ縦分割幅設定
 
-	float& GetAlpha();				// アルファ値取得
-	void SetAlpha(float fAlpha);	// アルファ値設定
-
 	static CObject_2D* Create();	// 生成
 
 private:
@@ -63,13 +66,13 @@ private:
 	D3DXVECTOR3 m_pos;	// 位置
 	D3DXVECTOR3 m_rot;	// 向き
 	D3DXVECTOR3 m_size;	// サイズ
+	D3DXCOLOR m_col;	// 色
 	float m_fLength;	// 展開用対角線
 	float m_fAngle;		// 対角線用角度
 	float m_fTexWidth;	// 横テクスチャ分割幅
 	float m_fTexHeight;	// 縦テクスチャ分縦幅
 	int m_nNowPatternU;	// 現在の横テクスチャ種類
 	int m_nNowPatternV;	// 現在の縦テクスチャ種類
-	float m_fAlpah;		// アルファ値
 };
 
 #endif // _OBJECT_2D_H_
